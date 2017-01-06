909715ac14676e702d4bf3eacc0f77ac2aedc71c
Andrew Zyabin <schastlilya@gmail.com>
2017-01-06 21:32:45 +0300

    Add cobblestone and grass block

--- mof/init.lua
+++ mof/init.lua
@@ -1,1 +1,27 @@
-print("mof version 1/161224 up.")
+print("mof version 1/170106 up.")
+
+minetest.register_node("mof:cobblestone", {
+  description = "Cobblestone",
+  tiles = {
+    "mof_cobblestone.png",
+    "mof_cobblestone.png",
+    "mof_cobblestone.png",
+    "mof_cobblestone.png",
+    "mof_cobblestone.png",
+    "mof_cobblestone.png"
+  },
+  groups = {cracky = 3}
+})
+
+minetest.register_node("mof:grass", {
+  description = "Grass block",
+  tiles = {
+    "mof_grass.png",
+    "mof_grass.png",
+    "mof_grass.png",
+    "mof_grass.png",
+    "mof_grass.png",
+    "mof_grass.png"
+  },
+  groups = {crumbly = 2}
+})
